1a67edc655884e8a11e8b4a619d53afcbd9fa341
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2025-11-05 09:06:22 -0600

    Clases

---
+++ 31_clases.py
@@ -0,0 +1,24 @@
+#Definición de una Clase
+class Persona:
+    def inicializarPersona(self, nombre, apellido):
+        #Creamos los atributos de la clase
+        self.nombre = nombre
+        self.apellido = apellido
+    
+    def mostrarPersona(self):
+        print('Datos de la Persona')
+        print(f'Nombre: {self.nombre}')
+        print(f'Apellido: {self.apellido}')
+    
+
+#Creación de Objetos por medio de la Instancia
+persona = Persona() #Crea un Objeto Vacio en Memoria
+nombre = input('Nombre: ')
+apellido = input('Apellido: ')
+persona.inicializarPersona(nombre, apellido)
+persona.mostrarPersona()
+personaDos = Persona()
+personaDos.inicializarPersona('Pedro', 'Juárez')
+personaDos.mostrarPersona()
+
+
